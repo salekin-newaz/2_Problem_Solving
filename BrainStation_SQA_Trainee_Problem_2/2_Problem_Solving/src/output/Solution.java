f48c28f98fbf13429b807414e820a465e86d1eb1
salekin-newaz <salekinnewaz0@gmail.com>
2021-11-12 23:26:56 +0600

    FirstCommit

---
+++ BrainStation_SQA_Trainee_Problem_2/2_Problem_Solving/src/output/Solution.java
@@ -0,0 +1,31 @@
+package output;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List<Student> studentList = new ArrayList<Student>();
+      while(testCases>0){
+         int id = in.nextInt();
+         String fname = in.next();
+         double cgpa = in.nextDouble();
+         
+         Student st = new Student(id, fname, cgpa);
+         studentList.add(st);
+         
+         testCases--;
+      }
+      Collections.sort(studentList,  Comparator.comparing(Student :: getCgpa).reversed().thenComparing(Student :: getFname).thenComparing(Student :: getId));
+         for(Student st: studentList){
+         System.out.println(st.getFname());
+      }
+   }
+}
